fa60bf0b8947d87f91856b66dcfe039431c90a4b
Tessa1217 <kwonyuj1996@gmail.com>
2025-09-14 05:06:51 +0900

    update : add 404 path

--- spa-no-router/src/App.tsx
+++ spa-no-router/src/App.tsx
@@ -5,7 +5,7 @@ import Route from "./router/Route";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
 import PostDetail from "./pages/PostDetail";
-
+import NotFound from "@/pages/NotFound";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +14,8 @@ function App() {
           <Route index={true} element={<Home />} />
           <Route path="posts" element={<Posts />} />
           <Route path="posts/:id" element={<PostDetail />} />
+          {/* 404 Page */}
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
