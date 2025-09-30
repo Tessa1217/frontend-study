4bcb0fff8ead0681c42589139a134d6e8be487fe
Tessa1217 <kwonyuj1996@gmail.com>
2025-10-01 03:01:44 +0900

    feat(dnd): drag and drop 관련 로직 별도 훅으로 분리

--- poc-dnd/src/App.tsx
+++ poc-dnd/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import { DndContext, type DragEndEvent } from "@dnd-kit/core";
 import { type Node } from "./types";
 import RootDropZone from "./components/RootDropZone";
+import { useDragAndDropNode } from "./hooks/useDragAndDropNode";
 
 const initialTree: Node[] = [
   {
@@ -13,87 +13,7 @@ const initialTree: Node[] = [
   { id: "3", type: "Input", acceptsChildren: false },
 ];
 function App() {
-  const [tree, setTree] = useState<Node[]>(initialTree);
-
-  const moveNode = (parentId: string | null, childId: string) => {
-    if (parentId === childId) return;
-
-    setTree((prev) => {
-      const copy: Node[] = structuredClone(prev);
-
-      // parent가 children 허용하지 않으면 무시
-      if (parentId) {
-        const findParent = (nodes: Node[], id: string): Node | null => {
-          for (const n of nodes) {
-            if (n.id === id) return n;
-            if (n.children) {
-              const found = findParent(n.children, id);
-              if (found) return found;
-            }
-          }
-          return null;
-        };
-        const parentNode = findParent(copy, parentId);
-        if (parentNode && !parentNode.acceptsChildren) {
-          return prev; // ❌ children 불가 parent → 이동 취소
-        }
-      }
-
-      // 제거
-      const removeNode = (nodes: Node[], id: string): [Node | null, Node[]] => {
-        const next: Node[] = [];
-        let removed: Node | null = null;
-
-        for (const n of nodes) {
-          if (n.id === id) {
-            removed = n;
-            continue;
-          }
-          if (n.children) {
-            const [r, newChildren] = removeNode(n.children, id);
-            if (r) {
-              removed = r;
-              next.push({ ...n, children: newChildren });
-            } else {
-              next.push(n);
-            }
-          } else {
-            next.push(n);
-          }
-        }
-        return [removed, next];
-      };
-
-      const insertNode = (
-        nodes: Node[],
-        parentId: string,
-        newNode: Node
-      ): Node[] => {
-        return nodes.map((n) => {
-          if (n.id === parentId) {
-            return { ...n, children: [...(n.children || []), newNode] };
-          }
-          if (n.children) {
-            return {
-              ...n,
-              children: insertNode(n.children, parentId, newNode),
-            };
-          }
-          return n;
-        });
-      };
-
-      const [removed, updated] = removeNode(copy, childId);
-      if (!removed) return prev;
-
-      // 루트로 이동 → 그냥 최상위 배열로 append
-      if (!parentId) {
-        return [...updated, removed];
-      }
-
-      return insertNode(updated, parentId, removed);
-    });
-  };
+  const { tree, moveNode } = useDragAndDropNode(initialTree);
 
   const handleDragEnd = (e: DragEndEvent) => {
     const { active, over } = e;
---
+++ poc-dnd/src/hooks/useDragAndDropNode.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { type Node } from "../types";
+
+export function useDragAndDropNode(initialTree: Node[]) {
+  const [tree, setTree] = useState<Node[]>(initialTree);
+
+  const moveNode = (parentId: string | null, childId: string) => {
+    if (parentId === childId) return; // 자기 자신인 경우
+    setTree((prev) => {
+      const copy: Node[] = structuredClone(prev);
+      if (parentId) {
+        const parentNode = findParent(copy, parentId);
+        if (parentNode && !parentNode.acceptsChildren) {
+          return prev; // ❌ children 불가 parent → 이동 취소 예) container 등 요소 포함 가능 노드가 아닌 일반 요소 노드
+        }
+      }
+
+      const [removed, updated] = removeNode(copy, childId);
+
+      if (!removed) return prev;
+
+      if (!parentId) {
+        return [...updated, removed];
+      }
+
+      return insertNode(updated, parentId, removed);
+    });
+  };
+
+  const findParent = (nodes: Node[], id: string): Node | null => {
+    for (const n of nodes) {
+      // 찾을 때까지 재귀로 탐색
+      if (n.id === id) return n;
+      if (n.children) {
+        const found = findParent(n.children, id);
+        if (found) return found;
+      }
+    }
+    return null;
+  };
+
+  // 노드 삭제
+  const removeNode = (nodes: Node[], id: string): [Node | null, Node[]] => {
+    const next: Node[] = [];
+    let removed: Node | null = null;
+
+    for (const n of nodes) {
+      if (n.id === id) {
+        removed = n;
+        continue;
+      }
+
+      if (n.children) {
+        const [r, newChildren] = removeNode(n.children, id);
+        if (r) {
+          removed = r;
+          next.push({ ...n, children: newChildren });
+        } else {
+          next.push(n);
+        }
+      } else {
+        next.push(n);
+      }
+    }
+
+    return [removed, next];
+  };
+
+  // 노드 추가
+  const insertNode = (
+    nodes: Node[],
+    parentId: string,
+    newNode: Node
+  ): Node[] => {
+    return nodes.map((n) => {
+      if (n.id === parentId) {
+        return { ...n, children: [...(n.children || []), newNode] };
+      }
+      if (n.children) {
+        return {
+          ...n,
+          children: insertNode(n.children, parentId, newNode),
+        };
+      }
+      return n;
+    });
+  };
+
+  return {
+    moveNode,
+    tree,
+  };
+}
